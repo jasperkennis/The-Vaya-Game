2ba40ee68e9e91fbcba4b8c222c0cde2c803e6b1
Jasper Kennis <jasper@creativetoken.nl>
2011-09-15 15:41:16 +0200

    Wrote the initial logics and connected the library to the project.

--- src/nl/vaya/mobilegame/TheVayaGameActivity.java
+++ src/nl/vaya/mobilegame/TheVayaGameActivity.java
@@ -1,13 +1,28 @@
 package nl.vaya.mobilegame;
 
+import org.cocos2d.opengl.CCGLSurfaceView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class TheVayaGameActivity extends Activity {
+	
+	protected CCGLSurfaceView _glSurfaceView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	    super.onCreate(savedInstanceState);
+	        
+	     // Set screen settings:
+	    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	    
+	    // Set content:
+        _glSurfaceView = new CCGLSurfaceView(this);
+        setContentView(_glSurfaceView);
     }
 }
