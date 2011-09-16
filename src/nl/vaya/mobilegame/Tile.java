876e77b1022d51214bc593e16e4927700a9cf8b4
Jasper Kennis <jasper@creativetoken.nl>
2011-09-16 12:21:25 +0200

    Added some basic methods to the tile class

--- src/nl/vaya/mobilegame/Tile.java
+++ src/nl/vaya/mobilegame/Tile.java
@@ -7,34 +7,52 @@ import org.cocos2d.nodes.CCSprite;
 public class Tile {
 
 	protected ArrayList<CCSprite> _sprites;
+	protected CCSprite _activeSprite;
 	
 	
 	/**
 	 * Accepts an array of sprites and stores them in the array list of sprites
 	 * @param sprites
 	 */
-	public void Tile(CCSprite[] sprites) {
+	public Tile(CCSprite[] sprites) {
 		
 		_sprites = new ArrayList<CCSprite>();
 		
 		for (CCSprite sprite : sprites){
 			addSprite(sprite);
 		}
+		
+		_activeSprite = _sprites.get(0);
 	}
 	
 	/**
 	 * Accepts a single sprite and stores it in the array list of sprites.
 	 * @param sprite
 	 */
-	public void Tile(CCSprite sprite) {
+	public Tile(CCSprite sprite) {
 		
 		_sprites = new ArrayList<CCSprite>();
 		
 		addSprite(sprite);
+		
+		_activeSprite = _sprites.get(0);
 	}
 	
 	
 	public void addSprite(CCSprite sprite){
 		_sprites.add(sprite);
+	}
+	
+	
+	public CCSprite getTile(){
+		return _activeSprite;
+	}
+	
+	public void update(){
+		if(_sprites.size() < 2){
+			return;
+		}
+		
+		
 	}
 }
