7772d0741615f5363d7b474ca9135829d173a841
Remi Appels <mail@remii.nl>
2011-09-16 15:14:14 +0200

    Removed extra reference

--- src/nl/vaya/mobilegame/layer/FloorLayer.java
+++ src/nl/vaya/mobilegame/layer/FloorLayer.java
@@ -2,8 +2,6 @@ package nl.vaya.mobilegame.layer;
 
 import org.cocos2d.layers.CCLayer;
 
-import android.util.Log;
-
 
 public class FloorLayer extends CCLayer {
 	static final String logTag = "log_tag";                       
