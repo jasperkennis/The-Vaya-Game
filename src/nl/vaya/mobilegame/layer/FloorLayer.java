1ea5316419f046806fb28e1939d7ffd64d094bb5
Remi Appels <mail@remii.nl>
2011-09-16 14:08:21 +0200

    Merge warning

--- src/nl/vaya/mobilegame/layer/FloorLayer.java
+++ src/nl/vaya/mobilegame/layer/FloorLayer.java
@@ -6,20 +6,13 @@ import android.util.Log;
 
 
 public class FloorLayer extends CCLayer {
-	static final String logTag = "log_tag";
-	
-	//1 = Land                   __  __
-	//2 = Land - Water         /1  \/  \
-	//3 = Land - Water         \2       /
-	//4 =                      3|_/\__|
-	//                         
-	//                         
+	static final String logTag = "log_tag";                       
 	
 	public FloorLayer(){
 
 	}
 	
 	public void update(float dt){
-		//Log.i(logTag, "Update FloorLayer");
+		Log.i(logTag, "Update FloorLayer");
 	}
 }
