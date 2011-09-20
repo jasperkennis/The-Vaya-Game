a3d37af1a4682d91b90d105b7e36a96652dd4460
Jasper Kennis <jasper@creativetoken.nl>
2011-09-20 14:40:43 +0200

    We've been fucking about and everything sucks.

--- src/nl/vaya/mobilegame/layer/FloorLayer.java
+++ src/nl/vaya/mobilegame/layer/FloorLayer.java
@@ -2,18 +2,12 @@ package nl.vaya.mobilegame.layer;
 
 import nl.vaya.mobilegame.TileObject;
 
-import org.cocos2d.actions.base.CCRepeatForever;
-import org.cocos2d.actions.interval.CCAnimate;
 import org.cocos2d.layers.CCLayer;
-import org.cocos2d.nodes.CCAnimation;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCSprite;
-import org.cocos2d.nodes.CCSpriteFrame;
-import org.cocos2d.nodes.CCSpriteSheet;
-import org.cocos2d.nodes.CCTextureCache;
-import org.cocos2d.types.CGPoint;
-import org.cocos2d.types.CGRect;
 import org.cocos2d.types.CGSize;
+
+import android.util.Log;
 
 public class FloorLayer extends CCLayer {
 	static final String logTag = "log_tag";
@@ -72,31 +66,43 @@ public class FloorLayer extends CCLayer {
 		        13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13
 		};
 		
-		// Add a FloorSpriteSheetheet:
-		CCSpriteSheet FloorSpriteSheet = CCSpriteSheet.spriteSheet("FloorSpriteSheet.png");
-		addChild(FloorSpriteSheet);
+		_tileArray[1] = new TileObject(CCSprite.sprite("sand.png"));						//Tile object 1
+		Log.i(logTag, "1");
+		_tileArray[2] = new TileObject(CCSprite.sprite("water.png"));						//Tile object 2
+		Log.i(logTag, "2");
+		_tileArray[3] = new TileObject(CCSprite.sprite("zandrechtonder.png"));				//Tile object 3
+		Log.i(logTag, "3");
+		_tileArray[4] = new TileObject(CCSprite.sprite("zandrechtboven.png"));				//Tile object 4
+		Log.i(logTag, "4");
+		_tileArray[5] = new TileObject(CCSprite.sprite("hoekrechtsonder.png"));				//Tile object 5
+		Log.i(logTag, "5");
+		_tileArray[6] = new TileObject(CCSprite.sprite("hoeklinksonder.png"));				//Tile object 6
+		Log.i(logTag, "6");
+		_tileArray[7] = new TileObject(CCSprite.sprite("hoekrechtsboven.png"));				//Tile object 7
+		Log.i(logTag, "7");
+		_tileArray[8] = new TileObject(CCSprite.sprite("hoeklinksboven.png"));				//Tile object 8
+		Log.i(logTag, "8");
+		_tileArray[9] = new TileObject(CCSprite.sprite("binnenhoeklinksboven.png"));		//Tile object 9
+		Log.i(logTag, "9");
+		_tileArray[10] = new TileObject(CCSprite.sprite("binnenhoekrechtsboven.png"));		//Tile object 10
+		Log.i(logTag, "10");
+		_tileArray[11] = new TileObject(CCSprite.sprite("binnenhoeklinksonder.png"));		//Tile object 11
+		Log.i(logTag, "11");
+		_tileArray[12] = new TileObject(CCSprite.sprite("binnenhoekrechtsonder.png"));		//Tile object 12
+		Log.i(logTag, "12");
+		_tileArray[13] = new TileObject(CCSprite.sprite("zandrechtrechts.png"));			//Tile object 13
+		Log.i(logTag, "13");
+		_tileArray[14] = new TileObject(CCSprite.sprite("zandrechtlinks.png"));				//Tile object 14
+		Log.i(logTag, "14");
+		for(int i = 0; i< 4; i++){
+		//for(int i = 0; i< _tilePositionArray.length; i++){
+			float y = (int)(i/40);
+			float x = (int)i-(y*40);
+			_tileArray[_tilePositionArray[i]].setTileSize(_numTilesWidth, _numTilesHeight, winSize);
+			_tileArray[_tilePositionArray[i]].setPosition(x,y);
+			this.addChild(_tileArray[_tilePositionArray[i]].getTile());
 
-		// Add the waterSprite to the sheet
-		CCSprite waterSprite = CCSprite.sprite(FloorSpriteSheet.getTexture(), new CGRect(CGPoint.ccp(0.0f , 0.0f), CGSize.make(32.0f, 32.0f)));
-		FloorSpriteSheet.addChild(waterSprite);
-		
-		// Set waterSprite position
-		CGSize s = CCDirector.sharedDirector().winSize();
-		waterSprite.setPosition(CGPoint.ccp(s.width/2,s.height/2));
-		
-		// Create an animation
-		CCAnimation waterAnimation = CCAnimation.animation("waterAnimation", 0.3f);
-		
-		CCSpriteFrame waterFrame1 = CCSpriteFrame.frame(FloorSpriteSheet.getTexture(), new CGRect(CGPoint.ccp(0.0f,0.0f), CGSize.make(32.0f, 32.0f)), CGPoint.make(0.0f, 0.0f));
-		CCSpriteFrame waterFrame2 = CCSpriteFrame.frame(FloorSpriteSheet.getTexture(), new CGRect(CGPoint.ccp(32.0f,0.0f), CGSize.make(32.0f, 32.0f)), CGPoint.make(0.0f, 0.0f));
-		
-		waterAnimation.addFrame(waterFrame1);
-		waterAnimation.addFrame(waterFrame2);
-		
-		CCAnimate waterAction = CCAnimate.action(waterAnimation);
-		CCRepeatForever repeat = CCRepeatForever.action(waterAction);
-		
-		waterSprite.runAction(repeat);
+		}
 	}
 	
 	public void update(float dt){
