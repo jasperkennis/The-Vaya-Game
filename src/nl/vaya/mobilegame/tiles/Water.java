0cabbf0e5bea16c77d640caa5897f78a3077b032
Jasper Kennis <jasper@creativetoken.nl>
2011-09-22 13:10:06 +0200

    Created the new water object

---
+++ src/nl/vaya/mobilegame/tiles/Water.java
@@ -0,0 +1,16 @@
+package nl.vaya.mobilegame.tiles;
+
+import java.util.ArrayList;
+
+import org.cocos2d.nodes.CCSprite;
+
+public class Water extends TileObject {
+
+	public Water(CCSprite sprite) {
+		super(sprite);
+	}
+
+	public Water(CCSprite[] sprites) {
+		super(sprites);
+	}
+}
